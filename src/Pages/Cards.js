c481d9b337f8e39bde72861de1eda298c6890e09
deepak <dkp050897@gmail.com>
2022-12-03 06:58:43 +0530

    code

--- src/Pages/Cards.js
+++ src/Pages/Cards.js
@@ -3,21 +3,6 @@ import React from "react";
 import "./Cards.css";
 
 const RecipeCards = () => {
-  // const [getRecipes, setReipes] = useState([]);
-
-  // const Id = `5a25e98c`;
-  // const Key = "4bde6ba46c6550064a2f06008db2b33e";
-  // const url = `https://api.edamam.com/search?q=mango&app_id=${Id}&app_key=${Key}`;
-
-  // async function getData() {
-  //   let recipeData = await axios.get(url);
-  //   console.log(recipeData.data.hits);
-  //   setReipes(recipeData.data.hits);
-  // }
-
-  // useEffect(()=>{
-  //     getData()
-  // },[])
 
   return (
      
@@ -58,7 +43,7 @@ const RecipeCards = () => {
         <div className="cardss">
           <img src="../Images/front.jpg" alt="recipe" />
           <div className="cardss__info">
-            <span className="cardss__category">Chilli Recipe - 1</span>
+            <span className="cardss__category">Chilli Recipe - 2</span>
           </div>
         </div>
       </div>
